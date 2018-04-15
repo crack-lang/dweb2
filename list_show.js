ccdab84160fdd82b0467a0d77fe4994903a74cb3
Michael Muller <mmuller@enduden.com>
2018-04-15 13:57:18 -0400

    Add the ability to clear the list, expand the template, and remove unselected items.

--- list_show.js
+++ list_show.js
@@ -8,6 +8,13 @@ angular.module("list_show", [])
         $http.get('/list/get?name=mainlist')
             .then(function(response) {
                 $scope.main = response.data;
+
+                // Translate item.flags & FLAG_SELECTED to item.selected.
+                for (var i = 0; i < $scope.main.items.length; ++i) {
+                    var item = $scope.main.items[i];
+                    if (item.flags & FLAG_SELECTED)
+                        item.selected = true;
+                }
             });
 
         $http.get('/list/get?name=template')
@@ -38,6 +45,12 @@ angular.module("list_show", [])
             $scope.matches = [];
             $scope.selection = -1;
         }
+
+        function clone(obj) {
+            return JSON.parse(JSON.stringify(obj));
+        }
+
+        var FLAG_SELECTED = 1;
 
         $scope.specialKeys = function(event) {
             if (event.keyCode == 9 && $scope.matches.length > 0) {
@@ -91,7 +104,7 @@ angular.module("list_show", [])
                 if (i == $scope.template.items.length)
                     return false;
 
-                $scope.main.items.push(JSON.parse(JSON.stringify(item)))
+                $scope.main.items.push(clone(item))
                 clearMatches();
                 $scope.text = '';
                 return false;
@@ -100,9 +113,62 @@ angular.module("list_show", [])
         };
 
         $scope.saveList = function(event) {
+
+            // Translate the "selected" flag on the item to FLAG_SELECTED in
+            // the flags.
+            for (var i = 0; i < $scope.main.items.length; ++i) {
+                var item = $scope.main.items[i];
+                item.flags = item.selected ? FLAG_SELECTED : 0;
+            }
+
             $http.put('/list/put?name=mainlist', $scope.main)
                 .then(function() {
                       alert('List saved.');
                 });
         };
+
+        $scope.clearList = function(event) {
+            $scope.main.items = [];
+            $scope.saveList();
+        };
+
+        $scope.expandTemplate = function(event) {
+            var curItems = $scope.main.items;
+
+            function inCurItems(name) {
+                for (var i = 0; i < curItems.length; ++i) {
+                    if (curItems[i].name == name)
+                        return true;
+                }
+                return false;
+            }
+
+            // copy everything from the template that is either marked as
+            // selected in the template or explicitly selected in the user's
+            // list.
+            $scope.main.items = [];
+            for (var i = 0; i < $scope.template.items.length; ++i) {
+                var item = $scope.template.items[i];
+                if (item.flags & FLAG_SELECTED || inCurItems(item.name)) {
+                    item = clone(item);
+                    item.selected = false;
+                    $scope.main.items.push(item);
+                }
+            }
+
+            $scope.saveList();
+        };
+
+        $scope.removeUnselected = function(event) {
+            var curItems = $scope.main.items;
+            $scope.main.items = [];
+            for (var i = 0; i < curItems.length; ++i) {
+                var item = curItems[i];
+                if (item.selected)
+                    $scope.main.items.push(item);
+                item.selected = false;
+            }
+
+            $scope.saveList();
+        }
     });
